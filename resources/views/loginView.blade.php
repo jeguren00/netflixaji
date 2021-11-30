c6e07e44acbe985a09b41dc50f7858fcc8675a26
jeguren00 <jeguren00@gmail.com>
2021-11-30 16:52:47 +0100

    creat arxiu rutes complet i fet plantilla del login

---
+++ resources/views/loginView.blade.php
@@ -0,0 +1,59 @@
+<head>
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: #000000;
+            height: 100vh;
+        }
+        #login .container #login-row #login-column #login-box {
+            margin-top: 120px;
+            max-width: 600px;
+            height: 320px;
+            border: 1px solid #9C9C9C;
+            background-color: #ff0000;
+        }
+        #login .container #login-row #login-column #login-box #login-form {
+            padding: 20px;
+        }
+        #login .container #login-row #login-column #login-box #login-form #register-link {
+            margin-top: -85px;
+        }
+    </style>
+</head>
+<!------ Include the above in your HEAD tag ---------->
+
+<body>
+    <div id="login">
+        <h3 class="text-center text-white pt-5">Login StreamingAJI</h3>
+        <div class="container">
+            <div id="login-row" class="row justify-content-center align-items-center">
+                <div id="login-column" class="col-md-6">
+                    <div id="login-box" class="col-md-12">
+                        <form id="login-form" class="form" action="" method="post">
+                            <h3 class="text-center text-white">Login</h3>
+                            <div class="form-group">
+                                <label for="username" class="text-white">Username:</label><br>
+                                <input type="text" name="username" id="username" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="password" class="text-white">Password:</label><br>
+                                <input type="text" name="password" id="password" class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <label for="remember-me" class="text-white"><span>Remember me</span> <span><input id="remember-me" name="remember-me" type="checkbox"></span></label><br>
+                                <input type="submit" name="submit" class="btn btn-light btn-md " value="submit">
+                            </div>
+                            <div id="register-link" class="text-right">
+                                <a href="#" class="text-white">Register here</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,87 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//home login regiter users function
+Route::get('/login', function () {
+    return view('loginView');
+});
+
+Route::get('/home', function () {
+    return view('homeUnregistered');
+});
+
+Route::get('/register', function () {
+    return view('registration');
+});
+
+Route::get('/login/confirmMail', function () {
+    return view('');
+});
+
+Route::get('/register/pago', function () {
+    return view('paymentForm');
+});
+
+Route::get('/register/save', function () {
+    return view('');
+});
+
+Route::get('/register/pago/save', function () {
+    return view('');
+});
+
+//THE streaming platform
+Route::get('/userHome', function () {
+    return view('userHome');
+});
+
+Route::get('/userHome', function () {
+    return view('userHome');
+});
+
+Route::get('/video?idVideo=?', function () {
+    return view('');
+});
+
+Route::get('/video?idVideo=?', function () {
+    return view('');
+});
+
+Route::get('/serach?AAAAAA', function () {
+    return view('');
+});
+
+Route::get('/favourites', function () {
+    return view('userFavourites');
+});
+
+Route::get('/user/changeData', function () {
+    return view('userDataChangerForm');
+}); 
+
+Route::get('/user/viewData', function () {
+    return view('userDataView');
+}); 
+
+Route::get('/user/changeBdData', function () {
+    return view('');
+});
+
+//admin part
+
+Route::get('/admin/listUsers', function () {
+    return view('adminListUsers');
+});
+
+Route::get('/admin/addVideo', function () {
+    return view('adminAddVideo');
+});
+
+Route::get('/adminBlockUser', function () {
+    return view('adminBlockUser');
+});
+
+Route::get('/admin/changeBdData', function () {
+    return view('adminChangeBdData');
+});
