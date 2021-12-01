a6ea56540755b10c38de4703b7d70f0d4e36ad09
iaw43568322 <iaw43568322@h10.informatica.escoladeltreball.org>
2021-12-01 18:46:24 +0100

    Vista lista peliculas en progreso

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -9,7 +9,7 @@
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 
-    <title>Proba</title>
+    <title>@yield('title')</title>
 
     <style>
         .nav-item:hover {
@@ -39,3 +39,25 @@
             </div>
         </div>  
     </header>
+
+    @yield('content')
+
+    <div class="container">
+        <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+            <p class="col-md-4 mb-0 text-muted">&copy; 2021 Company, Inc</p>
+
+            <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img src="https://img.icons8.com/external-kiranshastry-solid-kiranshastry/64/fa314a/external-movie-cinema-kiranshastry-solid-kiranshastry-3.png"/>
+            </a>
+
+            <ul class="nav col-md-4 justify-content-end">
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Inicio</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Películas</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Series</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Mis favoritas</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Mi perfil</a></li>
+            </ul>
+        </footer>
+    </div>
+</body>
+</html>
--- resources/views/moviesList.blade.php
+++ resources/views/moviesList.blade.php
@@ -1,5 +1,7 @@
-@include('layouts/header')
+@extends('layouts/layouts')
+@section('title', 'Movies')
 
+@section('content')
 <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
         <button type="button" data-bs-target="#myCarousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -8,7 +10,7 @@
     </div>
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <video class="img-fluid w-100" autoplay loop muted>
+            <video class="img-fluid w-100" style="height: 30em;" autoplay loop muted>
                 <source src="trailerInterstellar.mp4" type="video/mp4" />
             </video>
 
@@ -40,7 +42,7 @@
             </div>
         </div>
         <div class="carousel-item">
-            <video class="img-fluid" autoplay loop muted>
+            <video class="img-fluid w-100" style="height: 5em" autoplay loop muted>
                 <source src="https://mdbootstrap.com/img/video/Tropical.mp4" type="video/mp4" />
             </video>
 
@@ -66,4 +68,123 @@
     </button>
 </div>
 
-@include('layouts/footer')
+<!-- Movies Carousel -->
+<div class="container">
+    <div class="row">
+        <div id="carouselMovies" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <div class="carousel-item active" data-interval="1000">
+                    <div class="card bg-danger d-block w-100">
+                        <img src="..." class="card-img-top">
+                        <div class="card-body">
+                            <h1 class="card-title">Hola</h1>
+                            <p class="card-text">vsfdsfvf</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" data-interval="1000">
+                    <div class="card bg-primary d-block w-100">
+                        <img src="..." class="card-img-top">
+                        <div class="card-body">
+                            <h1 class="card-title">Hocvdasvfsla</h1>
+                            <p class="card-text">vsfdsfvf</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="carousel-item" data-interval="1000">
+                    <div class="card bg-black d-block w-100">
+                        <img src="..." class="card-img-top">
+                        <div class="card-body">
+                            <h1 class="card-title">Hovsfdsfvla</h1>
+                            <p class="card-text">vsfdsfvf</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- Carousel controls -->
+            <button class="carousel-control-prev" data-bs-target="#carouselMovies" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </button>
+            <button class="carousel-control-next" data-bs-target="#carouselMovies" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </button>
+        </div>
+        <div class="col col-3 m-2">
+            <div class="card bg-danger">
+                <img src="..." class="card-img-top">
+                <div class="card-body">
+                    <h1 class="card-title">Hola</h1>
+                    <p class="card-text">vsfdsfvf</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col col-3 m-2">
+            <div class="card bg-primary">
+                <img src="..." class="card-img-top">
+                <div class="card-body">
+                    <h1 class="card-title">Hola</h1>
+                    <p class="card-text">vsfdsfvf</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="col col-3 m-2">
+            <div class="card bg-black">
+                <img src="..." class="card-img-top">
+                <div class="card-body">
+                    <h1 class="card-title">Hola</h1>
+                    <p class="card-text">vsfdsfvf</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- <div id="carousel2" class="container mt-5 mb-5">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carousel2" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carousel2" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carousel2" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel slide">
+        <div class="carousel-item active">
+            <div class="row">
+                <div class="col col-4">
+                    <h1>vcwsgvfd</h1>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="row">
+                <div class="col col-4">
+                    <h1>vcwsgvfd</h1>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="row">
+                <div class="col col-4">
+                    <h1>vcwsgvfd</h1>
+                </div>
+            </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carousel2" data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carousel2" data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+        </button>
+    </div>
+</div> -->
+
+
+@endsection
