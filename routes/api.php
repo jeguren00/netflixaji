3b5a49109072ab4d86a31f7504e459414ed5fdc9
jeguren00 <jeguren00@gmail.com>
2022-02-14 20:04:56 +0100

    fetes coses

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
+use App\Http\Resources\VideoCollection;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Video;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+//2. List all series
+Route::get('/video', function () {
+    return new VideoCollection(Video::Series());
 });
