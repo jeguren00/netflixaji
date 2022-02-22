0265a763979c73b2657af88be018a1221ddd0be0
iawadancuestamolina <1hiawcuestamolinaadan@gmail.com>
2022-02-22 17:48:44 +0100

    iniciando graphql adan

--- app/Models/Video.php
+++ app/Models/Video.php
@@ -38,12 +38,12 @@ class Video extends Model
      */
     public $timestamps = false;
 
-    protected $fillable = [
-        'title',
-        'type',
-        'season',
-        'chapter',
-    ];
+    // protected $fillable = [
+    //     'title',
+    //     'type',
+    //     'season',
+    //     'chapter',
+    // ];
 
     public function scopeType($query,$type){
         return $query->where('type',$type);   
